43f7338d89fc756d322167820544dff2c9070cee
Damien Rembert <damien.rembert@gmail.com>
2025-07-19 23:32:06 +0100

    adapt copied file

--- src/pages/year-on-year.tsx
+++ src/pages/year-on-year.tsx
@@ -30,12 +30,12 @@ const YearOnYearPage = ({ data }) => {
   const usage23: number[] = [];
   const usage24: number[] = [];
   const usage25: number[] = [];
-  let weekNumber: number = 1;
+  let weekNumber: number = 0;
 
-  data.allPostcodeCsv.edges.forEach(
+  data.allGenderCsv.edges.forEach(
     (item: {
       node: {
-        Count: number;
+        Count_sum: number;
         StartDate_first: string;
         EndDate_first: string;
       };
@@ -43,21 +43,21 @@ const YearOnYearPage = ({ data }) => {
       // labels.push(item.node.StartDate_first + " to " + item.node.EndDate_first);
 
       if (item.node.EndDate_first.endsWith("2021")) {
-        usage21.push(item.node.Count);
-        labels.push("Week " + weekNumber++);
+        usage21.push(item.node.Count_sum);
       }
       if (item.node.EndDate_first.endsWith("2022")) {
-        usage22.push(item.node.Count);
+        labels.push("Week " + ++weekNumber);
+        usage22.push(item.node.Count_sum);
       }
       if (item.node.EndDate_first.endsWith("2023")) {
-        usage23.push(item.node.Count);
+        usage23.push(item.node.Count_sum);
       }
       if (item.node.EndDate_first.endsWith("2024")) {
-        usage24.push(item.node.Count);
+        usage24.push(item.node.Count_sum);
       }
 
       if (item.node.EndDate_first.endsWith("2025")) {
-        usage25.push(item.node.Count);
+        usage25.push(item.node.Count_sum);
       }
     }
   );
@@ -79,64 +79,80 @@ const YearOnYearPage = ({ data }) => {
   const chartData = {
     labels,
     datasets: [
+
+
+      
+      
       {
         label: "2021",
         data: usage21,
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        borderColor: "rgb(0, 63, 92)",
+        backgroundColor: "rgba(0, 63, 92, 0.5)",
+        // borderColor: "rgb(151, 208, 251)",
+        // backgroundColor: "rgba(151, 208, 251, 0.5)",
         // stack: "total",
       },
       {
         label: "2022",
         data: usage22,
-        borderColor: "rgb(174, 255, 99)",
-        backgroundColor: "rgba(174, 255, 99, 0.5)",
+        borderColor: "rgb(88, 80, 141)",
+        backgroundColor: "rgba(88, 80, 141, 0.5)",
+        // borderColor: "rgb(117, 173, 214)",
+        // backgroundColor: "rgba(117, 173, 214, 0.5)",
       },
-
+      
       {
         label: "2023",
         data: usage23,
-        borderColor: "rgb(255, 99, 250)",
-        backgroundColor: "rgba(255, 99, 250, 0.5)",
+        borderColor: "rgb(255, 99, 97)",
+        backgroundColor: "rgba(255, 99, 97, 0.5)",
+        // borderColor: "rgb(83, 139, 177)",
+        // backgroundColor: "rgba(83, 139, 177, 0.5)",
       },
-
+      
       {
         label: "2024",
         data: usage24,
-        borderColor: "rgb(99, 156, 255)",
-        backgroundColor: "rgba(99, 156, 255, 0.5)",
+        borderColor: "rgb(255, 166, 0)",
+        backgroundColor: "rgba(255, 166, 0, 0.5)",
+      // borderColor: "rgb(48, 107, 142)",
+      // backgroundColor: "rgba(48, 107, 142, 0.5)",
       },
-
+    
+      
       {
         label: "2025",
         data: usage25,
-        borderColor: "rgb(255, 232, 99)",
-        backgroundColor: "rgba(255, 232, 99, 0.5)",
+        borderColor: "rgb(188, 80, 144)",
+        backgroundColor: "rgba(188, 80, 144, 0.5)",
+      // borderColor: "rgb(0, 70, 100)",
+      // backgroundColor: "rgba(0, 70, 100, 0.5)",
       },
     ],
   };
-
+  
   return (
     <>
       <Link to="/">Home</Link>
-      <Line options={options} data={chartData} />;
+      <Line options={options} data={chartData} />
     </>
   );
 };
 
 export const query = graphql`
   query MyQuery {
-    allPostcodeCsv {
+    allGenderCsv {
       edges {
         node {
           StartDate_first
           EndDate_first
-          Count
+          Count_sum
         }
       }
     }
   }
 `;
+
 
 export default YearOnYearPage;
 
