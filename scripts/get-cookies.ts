f08e77f086c692b3b34cf1bb89c30b741a4a5425
damien-rembert <damien.rembert@gmail.com>
2025-12-07 13:37:50 +0000

    remove test elements

--- scripts/get-cookies.ts
+++ scripts/get-cookies.ts
@@ -1,40 +1,48 @@
-import { test, expect } from '@playwright/test';
 import { chromium } from "playwright";
 import dotenv from "dotenv";
+import { exit } from "process";
 
 // TODO: run headless
-test("test", async () => {
-  dotenv.config();
+dotenv.config();
 
-  // console.log(process.env);
+// console.log(process.env);
 
-  let cookies = [];
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  try {
-    const page = await context.newPage();
+const gettingCookie = "Getting cookies";
+console.time(gettingCookie);
 
-    const username = process.env.MT_USERNAME ? process.env.MT_USERNAME : "";
-    const password = process.env.MT_PASSWORD ? process.env.MT_PASSWORD : "";
-    const baseUrl = process.env.MT_API_URL ? process.env.MT_API_URL : "";
+let cookies = [];
+const browser = await chromium.launch({ headless: true });
+const context = await browser.newContext();
+try {
+  const page = await context.newPage();
 
-    await page.goto(`${baseUrl}/library/myTurnLogin/auth`);
-    await page.getByRole("textbox", { name: "Username" }).click();
-    await page.getByRole("textbox", { name: "Username" }).fill(username);
-    await page.getByRole("textbox", { name: "Password" }).click();
-    await page.getByRole("textbox", { name: "Password" }).fill(password);
-    await page.getByRole("checkbox", { name: "Remember" }).check();
-    await page.getByRole("button", { name: "Login" }).click();
-    await expect(
-      page.getByRole("heading", { name: "Admin Dashboard" })
-    ).toBeVisible();
-    cookies = await context.cookies();
-    console.log("Cookies after logging in:", cookies);
-    // TODO: close the window
-  } catch (err) {
-    console.log(err);
-    await browser.close();
-    throw new Error();
+  const username = process.env.MT_USERNAME ? process.env.MT_USERNAME : "";
+  const password = process.env.MT_PASSWORD ? process.env.MT_PASSWORD : "";
+  const baseUrl = process.env.MT_API_URL ? process.env.MT_API_URL : "";
+
+  await page.goto(`${baseUrl}/library/myTurnLogin/auth`);
+  await page.getByRole("textbox", { name: "Username" }).click();
+  await page.getByRole("textbox", { name: "Username" }).fill(username);
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill(password);
+  await page.getByRole("checkbox", { name: "Remember" }).check();
+  await page.getByRole("button", { name: "Login" }).click();
+  const isLoggedIn = await page
+    .getByRole("heading", { name: "Admin Dashboard" })
+    .isVisible();
+  if (!isLoggedIn) {
+    console.error("failed to log in.");
+    exit();
   }
-});
+  await page.goto(`${baseUrl}/library/orgLoan/list`);
+  await page.getByRole("button", { name: "CSV" }).click();
 
+  cookies = await context.cookies();
+  console.log("Cookies after logging in:", cookies);
+  // TODO: close the window
+} catch (err) {
+  console.log(err);
+  await browser.close();
+  throw new Error();
+}
+console.timeEnd(gettingCookie);
