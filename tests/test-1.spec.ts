9831fa730ea46745ae4b72de2066aefed6f77a1e
damien-rembert <damien.rembert@gmail.com>
2025-12-07 01:32:42 +0000

    get cookies

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,29 +1,40 @@
 import { test, expect } from '@playwright/test';
-import dotenv from "dotenv"
-
-
-
+import { chromium } from "playwright";
+import dotenv from "dotenv";
 
 // TODO: run headless
-test('test', async ({ page }) => {
+test("test", async () => {
   dotenv.config();
 
   // console.log(process.env);
 
-  const username = process.env.MT_USERNAME ? process.env.MT_USERNAME : "";
-  const password = process.env.MT_PASSWORD ? process.env.MT_PASSWORD : "";
-  const baseUrl = process.env.MT_API_URL ? process.env.MT_API_URL : "";
+  let cookies = [];
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  try {
+    const page = await context.newPage();
 
-  await page.goto(`${baseUrl}/library/myTurnLogin/auth`);
-  await page.getByRole("textbox", { name: "Username" }).click();
-  await page.getByRole("textbox", { name: "Username" }).fill(username);
-  await page.getByRole("textbox", { name: "Password" }).click();
-  await page.getByRole("textbox", { name: "Password" }).fill(password);
-  await page.getByRole("checkbox", { name: "Remember" }).check();
-  await page.getByRole("button", { name: "Login" }).click();
-  await expect(
-    page.getByRole("heading", { name: "Admin Dashboard" })
-  ).toBeVisible();
-  // TODO: close the window
+    const username = process.env.MT_USERNAME ? process.env.MT_USERNAME : "";
+    const password = process.env.MT_PASSWORD ? process.env.MT_PASSWORD : "";
+    const baseUrl = process.env.MT_API_URL ? process.env.MT_API_URL : "";
+
+    await page.goto(`${baseUrl}/library/myTurnLogin/auth`);
+    await page.getByRole("textbox", { name: "Username" }).click();
+    await page.getByRole("textbox", { name: "Username" }).fill(username);
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).fill(password);
+    await page.getByRole("checkbox", { name: "Remember" }).check();
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(
+      page.getByRole("heading", { name: "Admin Dashboard" })
+    ).toBeVisible();
+    cookies = await context.cookies();
+    console.log("Cookies after logging in:", cookies);
+    // TODO: close the window
+  } catch (err) {
+    console.log(err);
+    await browser.close();
+    throw new Error();
+  }
 });
 
