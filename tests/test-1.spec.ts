013195a8f97ffdb4c20ba47d0cc0b7dd36666749
damien-rembert <damien.rembert@gmail.com>
2025-12-07 00:09:03 +0000

    login through playwright

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import dotenv from "dotenv"
+
+
+
+
+// TODO: run headless
+test('test', async ({ page }) => {
+  dotenv.config();
+  
+  // console.log(process.env);
+  
+  
+  const username = process.env.MT_USERNAME ? process.env.MT_USERNAME: "" ;
+  console.log(username);
+  const password = process.env.MT_PASSWORD ? process.env.MT_PASSWORD: "" ;
+  
+  
+  await page.goto('https://liverpooltoollibrary.myturn.com/library/myTurnLogin/auth');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.getByRole('checkbox', { name: 'Remember' }).check();
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Admin Dashboard' })).toBeVisible();
+  // TODO: close the window
+
+
+
+});
+
